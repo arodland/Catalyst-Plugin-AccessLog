6d218daf6c68d75b5fed8f41ad281882d4947b9f
Andrew Rodland <andrew@hbslabs.com>
2009-08-06 01:58:52 -0500

    Document!
    And a little cleanup...

--- lib/Catalyst/Plugin/AccessLog.pm
+++ lib/Catalyst/Plugin/AccessLog.pm
@@ -12,7 +12,6 @@ after 'setup_finalize' => sub { # Init ourselves
     formatter_class => 'Catalyst::Plugin::AccessLog::Formatter',
     time_format => '%Y-%m-%dT%H:%M:%S', # ISO8601-compatible
     time_zone => 'local',
-    enabled => 1,
     hostname_lookups => 0,
     target => \*STDERR,
     %$config
@@ -62,21 +61,116 @@ no Moose::Role;
 Requires Catalyst 5.8 or above.
 
     # In lib/MyApp.pm context
-    use Catalyst (qw<
+    use Catalyst qw(
         ConfigLoader
         -Stats=1
         AccessLog
         ... other plugins here ...
-    >);
-    with 'CatalystX::Logger';
+    );
+
+    __PACKAGE__->config(
+        'Plugin::AccessLog' => {
+            format => '%[time] %[remote_address] %[path] %[status] %[size]',
+            time_format => '%c',
+            time_zone => 'America/Chicago',
+        }
+    );
+
+    __PACKAGE__->setup();
 
 =head1 DESCRIPTION
 
-This isn't "debug" logging.  This is more like Apache access logs for the built
-in Catalyst server.
+This plugin isn't for "debug" logging. Instead it enables you to create
+"access logs" from within a Catalyst application instead of requiring a
+webserver to do it for you. It will work even with Catalyst debug logging
+turned off (but see NOTES below).
 
-The default format is the "Common Log Format".
+=head1 CONFIGURATION
 
-http://en.wikipedia.org/wiki/Common_Log_Format
+All configuration is optional; by default the plugin will log to STDERR in a
+format compatible with the "Common Log Format"
+(L<http://en.wikipedia.org/wiki/Common_Log_Format>).
+
+=over 4
+
+=item target
+
+B<Default:> C<\*STDERR>
+
+Where to log to. If C<target> is a filehandle or something that 
+C<< isa("IO::Handle") >>, lines of logging information will be C<print>ed to
+it. If C<target> is an object with an C<info> method it's assumed to be a
+logging object (e.g. L<Log::Dispatch> or L<Log::Log4perl>) and lines will be
+passed to the C<info> method. If it's a C<CODE> ref then it will be called
+with each line of logging output. If it's an unblessed scalar it will be
+interpreted as a filehandle and the plugin will try to open it for append
+and write lines to it.
+
+=item format
+
+B<Default:> C<'%h %l %u %t "%r" %s %b "%{Referer}i" "%{User-Agent}i"'> (Apache
+C<common> log format).
+
+The format string for each line of output. You can use Apache C<LogFormat>
+strings, with a reasonably good level of compatibility, or you can use a
+slightly more readable format. The log format is documented in detail in
+L<Catalyst::Plugin::AccessLog::Formatter>.
+
+=item time_format
+
+B<Default:> C<'%Y-%m-%dT%H:%M:%S'> (ISO 8601)
+
+The default time format for the C<%t> / C<%[time]> escape. This is a
+C<strftime> format string, which will be provided to L<DateTime>'s
+C<strftime> method.
+
+=item time_zone
+
+B<Default:> local
+
+The timezone to use when printing times in access logs. This will be passed
+to L<DateTime::TimeZone>'s constructor.
+
+=item formatter_class
+
+B<Default:> C<Catalyst::Plugin::AccessLog::Formatter>
+
+In case you want to do something completely different you may provide your
+own formatter class that implements the C<format_line> method and provide
+its name here.
+
+=item hostname_lookups
+
+B<Default:> B<false>
+
+If this option is set to a true value, then the C<%h> /
+C<%[remote_hostname]> escape will resolve the client IP address using
+reverse DNS. This is generally not recommended for reasons of performance
+and security. Equivalent to the Apache option C<HostnameLookups>.
+
+=back
+
+=head1 NOTES
+
+=head2 Request time statstics
+
+C<Catalyst::Plugin::AccessLog> works without regard to Catalyst's debug
+logging option. However, the C<%T> / C<%[handle_time]> escape is only
+available if Catalyst stats are enabled. By default, statistics are only
+collected in Catalyst if debugging is active. If you want to use the C<%T>
+escape you can enable stats by adding C<-Stats> to your C<use Catalyst>
+line or by setting the C<MYAPP_STATS> environment variable to 1.
+
+=head2 Logging to C<< $c->log >>
+
+It is generally not recommended to write the access log to C<< $c->log >>,
+especially if static file handling is enabled. However, there might be a
+good reason to do it somewhere. If the logging target is a coderef, it will
+receive C<$c> as its second argument. You can log to C<< $c->log >> with:
+
+    target => sub { pop->log->info(shift) }
+
+Don't store C<$c> anywhere that persists after the lifetime of the coderef
+or bad things will happen to you and everyone you know.
 
 =cut
--- lib/Catalyst/Plugin/AccessLog/Formatter.pm
+++ lib/Catalyst/Plugin/AccessLog/Formatter.pm
@@ -32,104 +32,6 @@ sub escape_string {
   return $str;
 }
 
-item ['a', 'remote_address'] => sub {
-  return shift->request->address;
-};
-
-item ['b', 'clf_size'] => sub {
-  return shift->response->content_length || "-";
-};
-
-item ['B', 'size'] => sub {
-  return shift->response->content_length;
-};
-
-item ['h', 'remote_hostname'] => sub {
-  my $c = shift;
-  if ($c->config->{'Plugin::AccessLog'}{hostname_lookups}) {
-    return $c->request->hostname;
-  } else {
-    return $c->request->address;
-  }
-};
-
-item 'l' => sub { # for apache compat
-  return "-";
-};
-
-item ['m', 'method'] => sub {
-  return shift->request->method;
-};
-
-item ['p', 'port'] => sub {
-  return shift->req->base->port;
-};
-
-item ['r', 'request_line'] => sub { # Mostly for apache's sake
-  my $c = shift;
-  return $c->req->method . " /" . $c->req->path . " " . $c->req->protocol;
-};
-
-item ['s', 'status'] => sub {
-  return shift->response->status;
-};
-
-item ['t', 'apache_time'] => sub {
-  my ($c, $arg) = @_;
-  my $config = $c->config->{'Plugin::AccessLog'};
-  my $format = $arg || '[%d/%b/%Y:%H:%M:%S %z]'; # Apache default
-  return DateTime->now(time_zone => $config->{time_zone})
-    ->strftime($format);
-};
-
-item ['time', 'datetime'] => sub {
-  my ($c, $arg) = @_;
-  my $config = $c->config->{'Plugin::AccessLog'};
-  my $format = $arg || $config->{time_format};
-
-  return DateTime->now(time_zone => $config->{time_zone})
-    ->strftime($format);
-};
-
-item ['u', 'remote_user'] => sub {
-  return shift->request->remote_user || '-';
-};
-
-item ['V', 'v', 'host_port'] => sub {
-  return shift->request->base->host_port;
-};
-
-item 'hostname' => sub {
-  return shift->request->base->host;
-};
-
-# Possibly improvement: use uri_for to absolutize this with base, and then
-# take the path component off of that...
-item ['U', 'path'] => sub {
-  return '/' . shift->request->path;
-};
-
-item ['T', 'handle_time'] => sub {
-  my $c = shift;
-  if ($c->use_stats) {
-    return sprintf "%f", $c->stats->elapsed;
-  } else {
-    return "-";
-  }
-};
-
-item ['i', 'header_apache'] => sub {
-  my ($c, $arg) = @_;
-  my $header = $c->req->header($arg);
-  return "-" unless defined($header) and length($header);
-  return escape_string($header);
-};
-
-item 'header' => sub {
-  my ($c, $arg) = @_;
-  return escape_string( $c->req->header($arg) );
-};
-
 sub get_item {
   my ($self, $c, $key, $arg) = @_;
 
@@ -167,6 +69,259 @@ sub format_line {
 
   return $output;
 }
+
+=head1 DESCRIPTION
+
+The log format argument is a string which will be used to generate each line
+of the access log. The string consists of literal characters which will be
+copied to the log output verbatim, and escapes, which will be replaced with
+information about the request or the response. This format string is
+intended to be compatible with the Apache C<LogFormat> directive, however it
+contains some extensions and leaves a few features unimplemented.
+
+Escapes can be either B<short escapes> or B<long escapes>. Both types begin
+with the "C<%>" character. Short escapes consist of a C<%> followed by a
+single character, for example C<%h> for the remote hostname. Long
+escapes consist of a C<%> followed by a name inside B<square brackets>, for
+example C<%[remote_hostname]> for the same option. Apache-compatible
+options have both short escapes and long escapes, while incompatible options
+have only long escapes.
+
+Some escapes (currently C<%[time]>, C<%[apache_time]>, C<%[header]>, and
+C<%[apache_header]>) may also take an argument, which can be optional or
+required. The argument is placed inside B<curly braces> between the percent
+sign and the name of the escape, for example C<%{User-agent}i> or
+C<%{User-agent}[header]> to get the value of the C<User-agent> header.
+
+A literal percent-sign can be produced in the output using the escape
+sequence C<%%>.
+
+=head2 Escapes
+
+=over 4
+
+=item %[remote_address], %a
+
+The IP address of the remote client.
+
+=cut
+
+item ['a', 'remote_address'] => sub {
+  return shift->request->address;
+};
+
+=item %[clf_size], %b
+
+The size of the response content in bytes. If the response content is empty,
+produces a dash C<-> instead of 0. This is compatible with CLF.
+
+=cut
+
+item ['b', 'clf_size'] => sub {
+  return shift->response->content_length || "-";
+};
+
+=item %[size], %B
+
+The size of the response content in bytes. Always numeric, even for 0.
+
+=cut
+
+item ['B', 'size'] => sub {
+  return shift->response->content_length;
+};
+
+=item %[remote_host], %h
+
+The hostname of the remote client, if the C<hostname_lookups> config option
+is true. Otherwise, the IP address of the remote client, as
+C<%[remote_address]>.
+
+=cut
+
+item ['h', 'remote_host'] => sub {
+  my $c = shift;
+  if ($c->config->{'Plugin::AccessLog'}{hostname_lookups}) {
+    return $c->request->hostname;
+  } else {
+    return $c->request->address;
+  }
+};
+
+=item %[header], %i
+
+The value of the request header named in the (mandatory) argument, or "-" if
+no such header was provided. Usage: C<%{User-agent}i> to get the
+C<User-agent> request header.
+
+=cut
+
+item ['i', 'header'] => sub {
+  my ($c, $arg) = @_;
+  my $header = $c->req->header($arg);
+  return "-" unless defined($header);
+  return escape_string($header);
+};
+
+=item %l
+
+For Apache compatibility, this option produces a single dash C<->. In Apache
+this option returns the remote username from an C<ident> check, if the
+module is present, which it never is, which means it always produces a
+single dash on Apache as well. We don't bother implementing ident.
+
+=cut
+
+item 'l' => sub { # for apache compat
+  return "-";
+};
+
+=item %[method], %m
+
+The request method (e.g. GET, POST).
+
+=cut
+
+item ['m', 'method'] => sub {
+  return shift->request->method;
+};
+
+=item %[port], %p
+
+The port number that the request was received on. In apache this is the
+server's "canonical port", however this is information that's not available
+to Catalyst.
+
+=cut
+
+item ['p', 'port'] => sub {
+  return shift->req->base->port;
+};
+
+=item %[request_line], %r
+
+The first line of the HTTP request, e.g. C<"GET / HTTP/1.0">.
+
+=cut
+
+item ['r', 'request_line'] => sub { # Mostly for apache's sake
+  my $c = shift;
+  return $c->req->method . " /" . $c->req->path . " " . $c->req->protocol;
+};
+
+=item %[status], %s
+
+The HTTP status of the response, e.g. 200 or 404.
+
+=cut
+
+item ['s', 'status'] => sub {
+  return shift->response->status;
+};
+
+=item %[apache_time], %t
+
+The time as the log line is being printed. Ideally, this would be the time
+the request was received, but this is currently unimplemented.
+
+While this escape and the C<%[time]> escape both take an optional
+C<strftime> argument, they differ in their default formats. This escape
+defaults to a "human readable" format which is lousy to parse, but is
+nonetheless compatible with apache.
+
+=cut
+
+item ['t', 'apache_time'] => sub {
+  my ($c, $arg) = @_;
+  my $config = $c->config->{'Plugin::AccessLog'};
+  my $format = $arg || '[%d/%b/%Y:%H:%M:%S %z]'; # Apache default
+  return DateTime->now(time_zone => $config->{time_zone})
+    ->strftime($format);
+};
+
+=item %[time], %[datetime]
+
+The time as the log line is being printed. Ideally, this would be the time
+the request was received, but this is currently unimplemented.
+
+While this escape and the C<%[apache_time]> escape both take an optional
+C<strftime> argument, they differ in their default formats. This escape
+defaults to the C<time_format> config option provided to
+C<Catalyst::Plugin::AccessLog>. If that option is not provided, the default
+is ISO 8601.
+
+=cut
+
+item ['time', 'datetime'] => sub {
+  my ($c, $arg) = @_;
+  my $config = $c->config->{'Plugin::AccessLog'};
+  my $format = $arg || $config->{time_format};
+
+  return DateTime->now(time_zone => $config->{time_zone})
+    ->strftime($format);
+};
+
+=item %[remote_user], %u
+
+The REMOTE_USER variable as set by HTTP basic auth, or certain frontend
+authentication methods. Returns a dash C<-> if no such thing exists.
+
+=cut
+
+item ['u', 'remote_user'] => sub {
+  return shift->request->remote_user || '-';
+};
+
+=item %[host_port], %v, %V
+
+The host and the port of the request URI. Apache specifies that these should
+be the server's "canonical" host and port, but this information is
+unavailable to Catalyst.
+
+=cut
+
+item ['V', 'v', 'host_port'] => sub {
+  return shift->request->base->host_port;
+};
+
+=item %[hostname]
+
+The hostname of the request URI.
+
+=cut
+
+item 'hostname' => sub {
+  return shift->request->base->host;
+};
+
+=item %[path], %U
+
+The request path (relative to the application root, but with a leading
+slash).
+
+=cut
+
+# Possibly improvement: use uri_for to absolutize this with base, and then
+# take the path component off of that...
+item ['U', 'path'] => sub {
+  return '/' . shift->request->path;
+};
+
+=item %[handle_time], %T
+
+The time spent handling this request, as provided by the C<< $c->stats >>
+object. Returns a dash C<-> if stats are unavailable.
+
+=cut
+
+item ['T', 'handle_time'] => sub {
+  my $c = shift;
+  if ($c->use_stats) {
+    return sprintf "%f", $c->stats->elapsed;
+  } else {
+    return "-";
+  }
+};
 
 no Moose;
 
